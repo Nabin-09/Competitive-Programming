d1ce0c481f158b51e8852752e8d0adaa6ee82d56
Nabin <sharmanabin.0910@gmail.com>
2024-07-12 12:22:15 +0530

    Game with integers

---
+++ CodeForces/14-Game-With-Integers/code.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n ;
+        cin>>n;
+        if((n%3 == 2)||(n%3 == 1)) {cout<<"First"<<endl;}
+        else {cout<<"Second"<<endl;}
+    }
+    return 0;
+}
